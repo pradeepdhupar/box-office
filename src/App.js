cbd78db28aa02f79b3549b58142e3dc4f91baac9
Pradeep Dhupar <dhuperpradeep262gmail.com>
2022-07-21 17:15:01 +0530

    added layout managment and created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Navs from "./components/Navs";
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
 function App() {
   return (
     <div>
-      <Navs />
       <Switch>
         <Route exact={true} path="/">
           <Home/>
@@ -14,7 +12,9 @@ function App() {
         <Route exact={true} path="/starred">
           <Starred/>
         </Route>
-        <Route>This is 404 page</Route>
+        <Route>
+          <div>This is 404 page</div>
+        </Route>
       </Switch>
     </div>
   );
