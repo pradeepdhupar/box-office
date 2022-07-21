8023ebeaca05fd3a2430e31844b49a58721cbae6
Pradeep Dhupar <dhuperpradeep262gmail.com>
2022-07-21 15:59:55 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      hii
-    </div>
-  );
+  return <Switch>
+      <Route exact={true} path="/">
+      This is home page
+      </Route>
+      <Route exact={true} path="/7">
+      Dhoni
+      </Route>
+      <Route>
+      This is 404 page
+      </Route>
+    </Switch>;
 }
 
 export default App;
