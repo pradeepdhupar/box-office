96dd43811a7a9098fdc70c7d22e54e5f80624b88
Pradeep Dhupar <dhuperpradeep262gmail.com>
2022-07-24 22:23:36 +0530

    star page and custom hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Starred from "./pages/Starred";
-import Show from "./pages/Show";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+import Show from './pages/Show';
+
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact={true} path="/">
-          <Home/>
-        </Route>
-        <Route exact={true} path="/starred">
-          <Starred/>
-        </Route>
-        <Route exact path="/show/:id">
-          <Show />
-        </Route>
-        <Route>
-          <div>This is 404 page</div>
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
   );
 }
 
