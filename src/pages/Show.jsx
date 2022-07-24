de9cccdcd44199a906158ff74c9afbc37fe6c0a7
Pradeep Dhupar <dhuperpradeep262gmail.com>
2022-07-24 12:02:44 +0530

    styled show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -5,6 +5,7 @@ import Details from "../components/show/Details";
 import Seasons from "../components/show/Seasons";
 import ShowMainData from "../components/show/ShowMainData";
 import { apiGet } from "../misc/config";
+import { InfoBlock, ShowPageWrapper } from "./Show.styled";
 
 const reducer = (prevState, action) => {
   switch (action.type) {
@@ -55,7 +56,7 @@ const Show = () => {
     };
   }, [id]);
 
-  console.log("show", show);
+  // console.log("show", show);
 
   if (isLoading) {
     return <div>Data is being loaded</div>;
@@ -66,7 +67,7 @@ const Show = () => {
   }
 
   return (
-    <div>
+    <ShowPageWrapper>
       <ShowMainData
         image={show.image}
         name={show.name}
@@ -74,19 +75,19 @@ const Show = () => {
         summary={show.summary}
         tags={show.genres}
       />
-      <div>
+      <InfoBlock>
         <h2>Details</h2>
         <Details status={show.status} network={show.network} premiered={show.premiered} />
-      </div>
-      <div>
+      </InfoBlock>
+      <InfoBlock>
         <h2>Seasons</h2>
         <Seasons  seasons={show._embedded.seasons} />
-      </div>
-      <div>
+      </InfoBlock>
+      <InfoBlock>
         <h2>Cast</h2>
         <Cast cast={show._embedded.cast} />
-      </div>
-    </div>
+      </InfoBlock>
+    </ShowPageWrapper>
   );
 };
 
