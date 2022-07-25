2aa9c2f8a0a9da11c7b64b6251b747440a8ead85
Pradeep Dhupar <dhuperpradeep262gmail.com>
2022-07-25 11:21:04 +0530

    defined style-components for nav and Tile

--- src/components/Navs.jsx
+++ src/components/Navs.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import {useLocation } from "react-router-dom";
+import { LinkStyled, NavList } from "./Navs.style";
 const LINKS = [
-    {to : '/', text : 'Home'},
-    {to : '/starred', text : 'Starred'}
-]
+  { to: "/", text: "Home" },
+  { to: "/starred", text: "Starred" },
+];
 const Navs = () => {
+  const location = useLocation();
   return (
     <div>
-        <ul>
-            {LINKS.map(item => <li key={item.to}><Link to={item.to}>{item.text}</Link></li>)}
-        </ul>
+      <NavList>
+        {LINKS.map((item) => (
+          <li key={item.to}>
+            <LinkStyled to={item.to} className={item.to === location.pathname ? 'active' :''}>{item.text}</LinkStyled>
+          </li>
+        ))}
+      </NavList>
     </div>
-  )
-}
+  );
+};
 
-export default Navs
+export default Navs;
